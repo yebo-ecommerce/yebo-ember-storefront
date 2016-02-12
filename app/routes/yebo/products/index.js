5e9790d2f7e1e467bf62996a025d86ccbd95141f
yurifrl <yurifrl@hotmail.com>
2016-02-12 15:19:00 -0200

    Isolate packages

---
+++ app/routes/yebo/products/index.js
@@ -0,0 +1,11 @@
+// Ember!
+import Ember from 'ember';
+import SearchRoute from 'yebo-ember-storefront/mixins/search-route';
+
+/**
+ * Product index route
+ */
+export default Ember.Route.extend(SearchRoute, {
+  lalal: "asdasd",
+  asdasdsa: "asdasd"
+});
--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {
+    "yebo": {
+      apiHost: "http://localhost:3000",
+      namespace: "api/v2",
+      mount: "/",
+      productsPath: "products",
+      cartPath: "cart",
+      checkoutPath: "checkout",
+      ordersPath: "orders",
+      taxonsPath: "t",
+      thanksPath: "thanks"
+    }
+  };
 };
