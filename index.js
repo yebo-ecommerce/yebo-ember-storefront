49f64661a2454ebc5911818724c4e5f87df99af9
yurifrl <yurifrl@hotmail.com>
2016-04-11 18:45:05 -0300

    dependecies

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
     });
 
     app.import('vendor/register-storefront.js');
+    app.import(app.bowerDirectory + '/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
 
     var options = app.options['yebo-ember-storefront'] || {};
 
